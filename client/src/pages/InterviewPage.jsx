d601f4137810fa65accceb655fcb00b863d34b6f
Mansi Kumari <160772861+mansijha831@users.noreply.github.com>
2026-04-19 03:59:48 +0530

    Update InterviewPage.jsx

--- client/src/pages/InterviewPage.jsx
+++ client/src/pages/InterviewPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import Step1SetUp from "../components/Step1SetUp";
+import Step1SetUP from "../components/Step1SetUP";
 import Step2Interview from "../components/Step2Interview";
 import Step3Report from "../components/Step3Report";
 
